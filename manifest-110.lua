269bafdea63f71eabbc9e69d6e8328af1627abe7
Linus Sjögren <thelinx@unreliablepollution.net>
2010-07-17 23:12:27 +0200

    more format changes

--- manifest-110.lua
+++ manifest-110.lua
@@ -1,12 +1,12 @@
 files = {
-	"background.png",
-	"conf.lua",
-	"leekspin.ogg",
-	"main.lua",
-	"metre.png",
-	"padding.lua",
-	"spinner.png",
-	"ualove/init.lua"
+	["background.png"],
+	["conf.lua"],
+	["leekspin.ogg"],
+	["main.lua"],
+	["metre.png"],
+	["padding.lua"],
+	["spinner.png"],
+	["ualove/init.lua"]
 }
 
 notes = [[
--- manifest-120.lua
+++ manifest-120.lua
@@ -1,16 +1,16 @@
 files = {
-	"background.png",
-	"conf.lua",
-	"leekspin.ogg",
-	"main.lua",
-	"metre.png",
-	"padding.lua",
-	"spinner.png",
-	"ualove/init.lua"
+        ["background.png"],
+        ["conf.lua"],
+        ["leekspin.ogg"],
+        ["main.lua"],
+        ["metre.png"],
+        ["padding.lua"],
+        ["spinner.png"],
+        ["ualove/init.lua"]
 }
 
 updated = {
-	"main.lua"
+	["main.lua"]
 }
 
 notes = [[
