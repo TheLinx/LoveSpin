533b4cddbcd2d1540aab44f7c47dce8209d3b262
Linus Sjögren <thelinx@unreliablepollution.net>
2010-07-17 22:23:12 +0200

    added UaLove update manifests thingies. more info later

---
+++ manifest-110.lua
@@ -0,0 +1,14 @@
+files = {
+	"background.png",
+	"conf.lua",
+	"leekspin.ogg",
+	"main.lua",
+	"metre.png",
+	"padding.lua",
+	"spinner.png",
+	"ualove/init.lua"
+}
+
+notes = [[
+Now uses UaLove back-end.
+]]
---
+++ manifest-120.lua
@@ -0,0 +1,18 @@
+files = {
+	"background.png",
+	"conf.lua",
+	"leekspin.ogg",
+	"main.lua",
+	"metre.png",
+	"padding.lua",
+	"spinner.png",
+	"ualove/init.lua"
+}
+
+updated = {
+	"main.lua"
+}
+
+notes = [[
+Updated to use UaLove's auto-update thingy
+]]
