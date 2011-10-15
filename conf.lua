59408763138ce8e81471022f4486737921f1b514
Linus Sjögren <thelinx@unreliablepollution.net>
2011-10-15 19:46:37 +0200

    Removed UaLove and LoveUpdate. Works for 0.7.2 too.
    Seriously, what the fuck was I thinking?

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+	t.screen.width = 1024
+	t.screen.height = 768
+	t.title = "LoveSpin"
+	t.author = "Linus Sjögren <thelinx@unreliablepollution.net>"
+	t.version = 072
+end
--- main.lua
+++ main.lua
@@ -1,124 +1,98 @@
--- LoveSpin
--- http://github.com/thelinx/lovespin
--- created by thelinx@unreliablepollution.net
-
-require("padding")
-
--- INITIAL FUNCTIONS
-
-hook.add("initial", function ()
-	love.graphics.setMode(1024, 768)
-	love.graphics.print("Loading...", 500, 350)
-end, "bootchange")
-
-hook.add("load", function()
+-- initialisation
+function love.load()
 	love.graphics.setFont(50)
-	game.images = {
+	images = {
 		background = love.graphics.newImage("background.png"),
 		spinner = love.graphics.newImage("spinner.png"),
-		gauge = love.graphics.newImage("metre.png")
+		gauge = love.graphics.newImage("gauge.png")
 	}
-	game.music = {
-		bgm = love.audio.newSource("leekspin.ogg", "stream")
-	}
-	game.leekangle = 0
-	game.oldangle = 0
-	game.laps = 0
-	game.lap = {false,false}
-	game.lpm = 0
-	game.state = "waiting"
-end, "init")
+	bgm = love.audio.newSource("leekspin.ogg", "stream")
+	leekangle = 0
+	oldangle = 0
+	laps = 0
+	lap = {false, false}
+	lpm = 0
+	state = "menu"
+end
 
--- DRAW FUNCTIONS
-
-function drawGameField()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.draw(game.images.background, 0, 30)
-	love.graphics.draw(game.images.spinner, 512, 390, game.leekangle, 1, 1, 333, 333)
-	local a = math.min(1, 1 - game.lpm / 400) * 692
+-- output pretty pictures
+function drawfield()
+	love.graphics.setColor(255, 255, 255, 255)
+	love.graphics.draw(images.background, 0, 30)
+	love.graphics.draw(images.spinner, 512, 390, leekangle, 1, 1, 333, 333)
+	local a = math.min(1, 1 - lpm / 400) * 692
 	love.graphics.setScissor(0, 30 + a, 1024, 692 - a)
-	love.graphics.draw(game.images.gauge, 0, 30)
+	love.graphics.draw(images.gauge, 0, 30)
 	love.graphics.setScissor()
 end
 
-hook.add("draw", drawGameField, "a-game-w", "waiting")
-hook.add("draw", drawGameField, "a-game-p", "playing")
-hook.add("draw", drawGameField, "a-game-s", "scores")
-
-hook.add("draw", function()
-	love.graphics.setColor(255, 102, 51)
-	love.graphics.rectangle("fill", 412, 130, 200, 100)
-	love.graphics.printf("SPIN THE LEEK AS FAST AS YOU CAN", 0, 400, 1024, "center")
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.printf("PLAY", 0, 195, 1024, "center")
-end, "dinst", "waiting")
-
-hook.add("draw", function()
-	love.graphics.setColor(70, 101, 153)
-	love.graphics.printf("YOU CAN STOP SPINNING NOW", 0, 350, 1024, "center")
-	love.graphics.printf("YOUR SCORE: "..math.floor(game.lpm).." spins per minute", 0, 450, 1024, "center")
-	love.graphics.printf("PRESS Q TO EXIT", 0, 550, 1024, "center")
-end, "dscores", "scores")
-
-hook.add("draw", function()
-	love.graphics.setColor(255, 255, 255)
-end, "z-clearcolour")
-
--- UPDATE FUNCTIONS
-
-hook.add("update", function()
-	if game.music.bgm:isStopped() then
-		game.stop()
-	else
-		local ang = math.atan2(love.mouse.getY()-384, love.mouse.getX()-512)
-		local fang = math.floor(ang/2)
-		if fang == 0 and game.lap[1] and game.lap[2] then
-			game.newLap()
-		end
-		if fang == 1 and not game.lap[1] then
-			game.lap[1] = true
-		end
-		if fang == -1 and not game.lap[2] then
-			game.lap[2] = true
-		end
-		game.leekangle = ang + (3.14 / 2)
-		game.lpm = game.laps * (60 / game.getTime())
+function love.draw()
+	drawfield()
+	if state == "menu" then
+		love.graphics.setColor(255, 102, 51)
+		love.graphics.rectangle("fill", 412, 130, 200, 100)
+		love.graphics.printf("SPIN THE LEEK AS FAST AS YOU CAN", 0, 350, 1024, "center")
+		love.graphics.setColor(255, 255, 255)
+		love.graphics.printf("PLAY", 0, 145, 1024, "center")
+	elseif state == "end" then
+		love.graphics.setColor(70, 101, 153)
+		love.graphics.printf("YOU CAN STOP SPINNING NOW", 0, 300, 1024, "center")
+		love.graphics.printf("YOUR SCORE: " .. math.floor(lpm) .. " spins per minute", 0, 400, 1024, "center")
+		love.graphics.printf("PRESS Q TO EXIT", 0, 550, 1024, "center")
 	end
-end, "gameupdate", "playing")
+end
 
-hook.add("keypressed", function(key)
-	if key == "q" or key == "escape" then
-		game.quit = true
-	end
-end, "qcheck")
-
-hook.add("mousepressed", function()
-	local x,y = love.mouse.getPosition()
-	if (x > 412 and x < 612) and
-	   (y > 130 and y < 230) then
-		game.start()
-	end
-end, "buttoncheck", "waiting")
-
--- GAME FUNCTIONS
-
-function game.start()
-	game.state = "playing"
+-- logic
+function start()
+	state = "playing"
 	love.mouse.setGrab(true)
-	love.audio.play(game.music.bgm)
-	game.starttime = love.timer.getTime()
+	love.audio.play(bgm)
+	starttime = love.timer.getTime()
 end
 
-function game.stop()
-	game.state = "scores"
-	love.mouse.setGrab(false)
+function stop()
+	state = "end"
+	love.mouse.setGrab("false")
 end
 
-function game.newLap()
-	game.lap = {false,false}
-	game.laps = game.laps + 1
+function newlap()
+	lap = {false, false}
+	laps = laps + 1
 end
 
-function game.getTime()
-	return love.timer.getTime() - game.starttime
+function gametime()
+	return love.timer.getTime() - starttime
+end
+
+function love.update(dt)
+	if state ~= "playing" then return end
+	if bgm:isStopped() then
+		stop()
+	else
+		local ang = math.atan2(love.mouse.getY() - 384, love.mouse.getX() - 512)
+		local fang = math.floor(ang / 2)
+		if fang == 0 and lap[1] and lap[2] then
+			newlap()
+		end
+		if fang == 1 and not lap[1] then
+			lap[1] = true
+		end
+		if fang == -1 and not lap[2] then
+			lap[2] = true
+		end
+		leekangle = ang + (math.pi / 2)
+		lpm = laps * (60 / gametime())
+	end
+end
+
+function love.keypressed(key)
+	if key == "q" or key == "escape" then
+		love.event.push("q")
+	end
+end
+
+function love.mousepressed(x, y)
+	if state == "menu" and x > 412 and x < 612 and y > 130 and y < 230 then
+		start()
+	end
 end
